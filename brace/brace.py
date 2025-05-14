84fd85b67ccde5893e83b61fbdb1891f1283dee5
Marcel Grund <grund.marcel@yahoo.de>
2025-05-14 21:49:22 +0200

    Cleaning

--- brace/brace.py
+++ brace/brace.py
@@ -7,23 +7,23 @@ from utils.rename_and_restructure_npz_files import rename_and_copy_npz_files
 from utils.test_dataset_labels import process_images
 
 
-videos_info_csv = 'dataset/base/videos_info.csv'
-segments_csv = 'dataset/base/segments.csv'
-sequences_csv = 'dataset/base/sequences.csv'
-annotated_all_segments_csv = 'dataset/annotated_all_segments.csv'
-annotated_powermove_segments_csv = 'dataset/annotated_powermove_segments.csv'
-annotated_toprock_segments_csv = 'dataset/annotated_toprock_segments.csv'
-annotated_footwork_segments_csv = 'dataset/annotated_footwork_segments.csv'
+videos_info_csv = 'BRACE_VIDEO_INFO_PATH'
+segments_csv = 'BRACE_SEGMENTS_PATH'
+sequences_csv = 'BRACE_SEQUENCES_PATH'
+annotated_all_segments_csv = 'DESTINATION_PATH'
+annotated_powermove_segments_csv = 'DESTINATION_PATH'
+annotated_toprock_segments_csv = 'DESTINATION_PATH'
+annotated_footwork_segments_csv = 'DESTINATION_PATH'
 
-manual_keypoints_folder = 'dataset/base/manual_keypoints'
-restructured_keypoints_folder = 'dataset/restructured_keypoints'
-brace_folder = 'dataset'
+manual_keypoints_folder = 'MANUAL_KEYPOINTS_FOLDER'
+restructured_keypoints_folder = 'DESTINATION_FOLDER_PATH'
+brace_folder = 'BRACE_FOLDER_PATH'
 dataset_destinaton_folder = 'DESTINATION_FOLDER'
 videos_folder = 'VIDEOS_FOLDER'
 frames_folder = 'FRAMES_FOLDER'
 
-dataset_folder = 'DATASET_FOLDER'
-test_dataset_folder = 'TEST_DATASET_FOLDER'
+dataset_folder = 'DESTINATION_DATASET_FOLDER'
+test_dataset_folder = 'DESTINATION_TEST_DATASET_FOLDER'
 
 def prepare_data():
     create_dance_type_csvs(segments_csv, brace_folder)
--- model_train/train_model.py
+++ model_train/train_model.py
@@ -3,10 +3,10 @@ import yaml
 
 MODEL_VERSION = 'MODEL_VERSION'
 DATA_PATH = 'data.yaml'
-EPOCHS = 100
-BATCH_SIZE = 8
-FREEZE_LAYER = 19
-IMAGE_SIZE = 960
+EPOCHS = "INT"
+BATCH_SIZE = "INT"
+FREEZE_LAYER = "INT"
+IMAGE_SIZE = "INT"
 
 def check_data_yaml(data_path):
     try:
@@ -21,7 +21,7 @@ def check_data_yaml(data_path):
 model = YOLO(MODEL_VERSION)
 
 def train_and_export(model, data_path, epochs, batch_size, image_size, freeze_layer, export_path='best.pt'):
-    model.train(data=data_path, epochs=epochs, batch=batch_size, imgsz = image_size, freeze=freeze_layer, multi_scale=True, optimizer='Adam', lr0=1e-5, device=0)
+    model.train(data=data_path, epochs=epochs, batch=batch_size, imgsz = image_size, freeze=freeze_layer, device=0)
 
     model.save(export_path)
     print(f"Model successfully exported:'{export_path}'")
